5ec54c2043b6702537e7764d0b6d20db622f97de
Gabriel <gabriel.lee@shell.com>
2023-12-02 07:45:52 +0000

    day 2 beautify

--- day-2/part-2.py
+++ day-2/part-2.py
@@ -8,11 +8,10 @@ for line in open('input.txt'):
         'green': 0,
         'blue': 0,
     }
-    for round in line.split(';'):
-        matches = re.findall(r'(\d+) (red|green|blue)', line)
-        for [count, color] in matches:
-            if int(count) > max[color]:
-                max[color] = int(count)
+    matches = re.findall(r'(\d+) (red|green|blue)', line)
+    for [count, color] in matches:
+        if int(count) > max[color]:
+            max[color] = int(count)
     total += max['red'] * max['green'] * max['blue']
 
 print(total)
